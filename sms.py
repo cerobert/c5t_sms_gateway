472cdfe3dd9577965ca9218798181d2b20502a07
crobert <crobert@caillot.fr>
2018-04-24 12:16:38 +0200

    [IMP]better comments and check status response

--- sms.py
+++ sms.py
@@ -28,17 +28,52 @@ class sms_gateway(osv.Model):
 
     _name = 'sms.gateway'
 
-    # Format phone number and check if len okay after format
+
+    def _check_status_code(self, status_code):
+        """
+        Take the status code and throw an exception if the server didn't return 200 or 201 code
+        @param status_code: status code returned by the server
+        @return: True or raise an exception
+        """
+        message_by_code = {204: 'No content',
+                           400: 'Bad Request',
+                           401: 'Unauthorized',
+                           404: 'Not Found',
+                           405: 'Method Not Allowed',
+                           500: 'Internal Server Error',}
+
+        error_label = ('This call to sms Gateway failed and '
+                       'returned an HTTP status of %d. That means: %s.')
+        if status_code in (200, 201):
+            return True
+        if status_code in message_by_code:
+            raise osv.except_osv(_('Warning!'), _(error_label % (status_code, message_by_code[status_code]), status_code))
+        else:
+            raise osv.except_osv(_('Warning!'), _(("This call to PrestaShop Web Services returned an unexpected HTTP status of: %d")  % (status_code,)))
+
+
     def get_phone_formatted(self,phone_number):
+        """
+        Format phone number and check if len okay after format
+        :param phone_number: str phone number
+        :return: formatted phone or False
+        """
         if not phone_number or not str(phone_number):
             return False
         phone_formatted = str(phone_number).replace('.','').replace('-','').replace('_','').strip()
         return len(phone_formatted) == 10 and phone_formatted.isdigit() and phone_formatted or False
 
 
-    # take a tuple or list or unique number and a message to send
     def send_message(self,cr,uid,phone_numbers,message,context=None):
-
+        """
+        Check params and send message via the smartphone gateway
+        :param cr: the current row, from the database cursor,
+        :param uid: the current user’s ID for security checks,
+        :param phone_numbers: tuple list or simple phone numbers
+        :param message: the message to send
+        :param context: A standard dictionary
+        :return:
+        """
         # We control everything
         if not message:
             raise osv.except_osv(_('Warning!'), _('No message was provided'))
@@ -63,7 +98,7 @@ class sms_gateway(osv.Model):
                 # http://192.168.1.66:58432/send.html?smsto=06XXXXXX&smsbody=tets&smstype=sms
                 requetesms = 'http://' + ip_sms_gateway + '/send.html?' + 'smsto=' + phone_formatted + '&smsbody=' + message + '&smstype=sms'
                 r = requests.get(requetesms)
-                r.status_code
+                self._check_status_code(r.status_code)
             except:
                 raise osv.except_osv(_('Warning!'),_('smthing went wrong while joining the gateway'))
 
